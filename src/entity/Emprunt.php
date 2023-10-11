574520fa4b340fb4f54e6a2e4ab00564676b0f72
Arturs Mednis <artursmednis2003@gmail.com>
2023-10-11 08:43:41 +0200

    methodes emprunt

--- src/entity/Emprunt.php
+++ src/entity/Emprunt.php
@@ -18,5 +18,34 @@ class Emprunt
 
     }
 
+    public function estEnCours():bool{
+        if (isset($this->dateEmprunt) && empty($this->dateRetour)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function setDateEmprunt(string $dateEmprunt): void
+    {
+        $date=\DateTime::createFromFormat("d/m/Y",$dateEmprunt);
+        $this->dateEmprunt = $date;
+    }
+
+    public function setDateRetourEstimee(string $dateRetourEstimee): void
+    {
+        $date=\DateTime::createFromFormat("d/m/Y",$dateRetourEstimee);
+        $this->dateRetourEstimee = $date;
+    }
+
+
+    public function estRetard():bool{
+        $dateActuelle=\DateTime::createFromFormat("d/m/Y",date("d/m/Y"));
+        if ($dateActuelle>$this->dateRetourEstimee && empty($this->dateRetour)){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 }
--- src/entity/Livre.php
+++ src/entity/Livre.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app;
+namespace App\entity;
 
 class Livre extends Media
 {
